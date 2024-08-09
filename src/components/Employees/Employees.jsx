8544f384c604671e09854ad9143b9f455e4210fa
Edina Hollo <edina.rostas8@gmail.com>
2024-08-09 10:23:03 -0700

    Add text animation

--- src/components/Employees/Employees.jsx
+++ src/components/Employees/Employees.jsx
@@ -2,8 +2,9 @@ import React, { useEffect, useState } from "react";
 import "./Employees.scss"; // Optional: for styling
 import dataObject from "../../data/employees.json";
 import Learn from "../Learn/Learn";
+import AnimatedText from "../AnimatedText/AnimatedText"; // Import the new component
 
-const prompts = dataObject.prompts; //array
+const prompts = dataObject.prompts; // Array
 
 const Employees = () => {
     const [selectedPrompt, setSelectedPrompt] = useState(null);
@@ -13,6 +14,7 @@ const Employees = () => {
     const [followUpId, setFollowUpId] = useState(null);
     const [followUpResponse, setFollowUpResponse] = useState("");
     const [closingMessage, setClosingMessage] = useState("");
+    const [explanationCompleted, setExplanationCompleted] = useState(false);
 
     const resetStates = () => {
         setSelectedPrompt(null);
@@ -21,20 +23,20 @@ const Employees = () => {
         setFollowUpResponse("");
         setConfirmation("");
         setClosingMessage("");
+        setExplanationCompleted(false);
     };
 
     const handleQuestionClick = (id) => {
         const promptData = prompts.find((p) => p.id === id);
         setResponse(promptData ? promptData.answer : "");
         setSelectedPrompt(id);
+        setExplanationCompleted(true);
 
         if (promptData.confirmation) {
             setConfirmation(promptData.confirmation);
             setFollowUp(promptData.followUp);
         } else {
-            setClosingMessage(
-                "Well Done! That’s how easy Generative AI is to use!"
-            );
+            setClosingMessage("Well Done! That’s how easy Generative AI is to use!");
         }
     };
 
@@ -42,16 +44,13 @@ const Employees = () => {
         const followUpData = followUp.find((f) => f.id === id);
         setFollowUpId(id);
         setFollowUpResponse(followUpData ? followUpData.answer : "");
-
-        setClosingMessage(
-            "Well Done! That’s how easy Generative AI is to use!"
-        );
+        setClosingMessage("Well Done! That’s how easy Generative AI is to use!");
     };
 
     return (
         <div className="employees-container">
             <div className="employees-card">
-                <Learn/>
+                <Learn />
                 <div className="prompt-response">
                     <p className="prompt-info">
                         choose a prompt question to see the response:{" "}
@@ -62,13 +61,12 @@ const Employees = () => {
                                 <tr key={option.id}>
                                     <td>
                                         <button
-                                            className={`question-button ${
-                                                selectedPrompt === option.id
-                                                    ? "selected"
-                                                    : selectedPrompt === null
+                                            className={`question-button ${selectedPrompt === option.id
+                                                ? "selected"
+                                                : selectedPrompt === null
                                                     ? ""
                                                     : "disabled"
-                                            }`}
+                                                }`}
                                             onClick={() =>
                                                 handleQuestionClick(option.id)
                                             }
@@ -85,23 +83,13 @@ const Employees = () => {
                         </tbody>
                     </table>
                     <div className="response-section">
-                        {response && (
-                             <p className="response-text">
-                             {selectedPrompt === 2 ? (
-                                 response.split("\n").map((line, index) => {
-                                     const [numberAndTitle, ...rest] = line.split(': ');
-                                     return (
-                                         <span key={index}>
-                                             <b>{numberAndTitle}:</b> {rest.join(': ')}<br />
-                                         </span>
-                                     );
-                                 })
-                             ) : (
-                                 response.split("\n").map((line, index) => (
-                                     <span key={index}>{line}<br /></span>
-                                 ))
-                             )}
-                            </p>
+                        {selectedPrompt && (
+                            <p className="explanation">{prompts.find((p) => p.id === selectedPrompt).explanation}</p>
+                        )}
+                        {explanationCompleted && (
+                            <AnimatedText
+                                text={response}
+                            />
                         )}
                     </div>
                     {confirmation && <p>{confirmation}</p>}
@@ -113,13 +101,12 @@ const Employees = () => {
                                     <tr key={option.id}>
                                         <td>
                                             <button
-                                                className={`question-button ${
-                                                    followUpId === option.id
-                                                        ? "selected"
-                                                        : followUpId === null
+                                                className={`question-button ${followUpId === option.id
+                                                    ? "selected"
+                                                    : followUpId === null
                                                         ? ""
                                                         : "disabled"
-                                                }`}
+                                                    }`}
                                                 onClick={() =>
                                                     handleFollowUpClick(
                                                         option.id
@@ -138,8 +125,13 @@ const Employees = () => {
                         </tbody>
                     </table>
                     <div className="response-section">
+                        {followUpId && (
+                            <p className="explanation">
+                                {followUp?.find((p) => p.id === followUpId).explanation}
+                            </p>
+                        )}
                         {followUpResponse && (
-                            <p className="response-text">{followUpResponse}</p>
+                            <AnimatedText text={followUpResponse} />
                         )}
                     </div>
                     {closingMessage && (
